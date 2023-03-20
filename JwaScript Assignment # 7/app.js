5ba16d91480a8730907984b5b13db4485953e838
SyedMudassir1 <122387591+SyedMudassir1@users.noreply.github.com>
2023-03-20 13:21:23 -0700

    Add files via upload

---
+++ JwaScript Assignment # 7/app.js
@@ -0,0 +1,85 @@
+// --------------> =========================== JawaScript Assignment # 7 ============================== <--------------------
+
+// Question # 1
+
+// var num = prompt("enter your number");
+// if (num === +num){
+//   document.write(num);
+// var round = Math.round(num);
+// document.write(" round off value: ",round);
+// document.write(" floor value: ",floor)
+// document.write("ceil value: ",ceil)
+// }
+// else{
+//   document.write("The Number is negitive integer");
+// }
+
+
+// Question # 2
+
+// var num = prompt("enter your number");
+// if (num === -num){
+//   document.write(num);
+// var round = Math.round(num);
+// document.write(" round off value: ",round);
+// document.write(" floor value: ",floor)
+// document.write("ceil value: ",ceil)
+// }
+// else{
+//   document.write("The Number is positive integer");
+// }
+
+
+// Question no 3
+
+// var num = -4;
+// var round = Math.abs(num);
+// document.write("The absolute value of -4 is ",round);
+
+
+// Question no 4
+ 
+// var num = 1;
+// var r = Math.random(num) * 6;
+// document.write("random dice value is  " + r );
+
+
+// Question no 5
+
+// var headuser = prompt("enter head username");
+// var tailsuser = prompt("enter tails username");
+
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss);
+
+// if(floor === 0){
+//   alert("Head " + headuser + " win the toss");
+// }else{
+//   alert("Tails " + tailsuser + " win the toss");
+// }
+
+// Question no 6
+
+//  var num = 1;
+//  var round = Math.random(num) * 100;
+//  document.write(round);
+
+
+// Question no 7 
+// var weight = prompt("Enter Your Weight");
+// alert("The weight of johar is  " + weight);
+
+
+// Question no 8
+
+// var num = prompt("enter number between 1-10");
+
+// var secretNum = 6;
+
+// if(num === "6"  ){
+//   alert("congratulations")
+// }else{
+//   alert("try again")
+// }
+ 
+// ---------------------------------------------------------------------- END ------------------------------------------------------------------ //
